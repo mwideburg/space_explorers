24c19e3f99902b75727f052bcf8b65acb14e2b15
rmiglin <rossmiglin@gmail.com>
2020-10-07 14:15:14 -0400

    add styling to story

--- frontend/src/components/story/story.js
+++ frontend/src/components/story/story.js
@@ -30,110 +30,123 @@ class Story extends React.Component {
         <div className="story-container">
           <Slider {...settings} className="slider">
             <div className="story-div">
-              <p className="story-text">
-                <p>
-                  In the year 2069 humanity was changed forever by a monumental
-                  discovery:
+              <div clasName="story-sub-div">
+                <p className="story-text">
+                  <p>
+                    In the year 2069 humanity was changed forever by a
+                    monumental discovery:
+                  </p>
+                  <p>
+                    Global warming and melting ice caps revealed an ancient
+                    intact alien ship that crashed in the arctic circle untold
+                    millennia ago.
+                  </p>
+                  World leaders battled for salvage rights but only one
+                  organization,
+                  <p className="Holtercorp">Holtercorp™,</p>
+                  <p className="Holtercorp-subtext">
+                    ended up with the technology.
+                  </p>
                 </p>
-                <p>
-                  Global warming and melting ice caps revealed an ancient intact
-                  alien ship that crashed in the arctic circle untold millennia
-                  ago.
-                </p>
-                World leaders battled for salvage rights but only one
-                organization,
-                <p className="Holtercorp">Holtercorp™,</p>
-                <p className="Holtercorp-subtext">
-                  ended up with the technology.
-                </p>
-              </p>
+              </div>
             </div>
             <div className="story-div">
-              <p className="story-text">
-                <p>
-                  While acquiring great profit, Holtercorp™ changed the world.
-                </p>
-                <p>
-                  Warp engines and micro-alloy synthesis allowed humanity to
-                  build spaceships and settle other planets in the solar system.
-                </p>
+              <div clasName="story-sub-div">
+                <p className="story-text">
+                  <p>
+                    While acquiring great profit, Holtercorp™ changed the world.
+                  </p>
+                  <p>
+                    Warp engines and micro-alloy synthesis allowed humanity to
+                    build spaceships and settle other planets in the solar
+                    system.
+                  </p>
 
-                <p>
-                  Hyperbolic incubation chambers and preserved alien fauna
-                  allowed humanity to create an infinite (if somewhat bland)
-                  supply of food.
+                  <p>
+                    Hyperbolic incubation chambers and preserved alien fauna
+                    allowed humanity to create an infinite (if somewhat bland)
+                    supply of food.
+                  </p>
+                  <img
+                    className="worldimage"
+                    src="https://c4.wallpaperflare.com/wallpaper/503/469/209/8-bit-city-buildings-artist-wallpaper-preview.jpg"
+                  />
+                </p>
+              </div>
+            </div>
+            <div className="story-div">
+              <div clasName="story-sub-div">
+                <p className="story-text">
+                  <p>
+                    For a brief time there was peace and prosperity, until
+                    inevitable human greed reshaped the solar system.
+                  </p>
+
+                  <img
+                    className="Rossimage"
+                    src="https://i.pinimg.com/originals/83/80/6c/83806c2d045fd0d7b0be73c6357708b9.png"
+                  />
+                  <p> A brilliant and evil engineer from Holtercorp™,</p>
+
+                  <p className="RosstheBoss">Ross Miglin</p>
+                </p>
+              </div>
+            </div>
+            <div className="story-div">
+              <div clasName="story-sub-div">
+                <p className="story-text">
+                  <p>
+                    The Evil Ross used alien computer technology and blockchain
+                    algorithms to devalue all the worlds currency and replaced
+                    it with Rosscoin.
+                  </p>
+                  <p>
+                    Overnight he became the wealthiest person in history and
+                    every nation on earth became a third world country.
+                  </p>
+                  <p>
+                    Ross used his infinite resources to build a mega fortress on
+                    Pluto and put a R60 Billion bounty on his own life, just to
+                    troll everyone.
+                  </p>
+                </p>
+              </div>
+            </div>
+            <div className="story-div">
+              <div clasName="story-sub-div">
+                <p className="story-text">
+                  <p>The world responded the only way it could:</p>
+                  <p>
+                    by devolving into a mercenary driven society ruled by
+                    whoever had the biggest and coolest giant robot.{" "}
+                  </p>
+                  <img
+                    className="mechimage"
+                    src="https://i.pinimg.com/originals/d7/ad/b0/d7adb06c596788cfbfbd86628d6b59ae.png"
+                  />
+                  <p>
+                    Perhaps one day a brave adventurer will build a robot so bad
+                    ass that they can travel all the way to Pluto and bring the
+                    world’s justice to Ross.
+                  </p>
+                </p>
+              </div>
+            </div>
+            <div className="story-div">
+              <div clasName="story-sub-div">
+                <p className="story-text">
+                  Until that day, humanity dreams and waits....
                 </p>
                 <img
-                  className="worldimage"
-                  src="https://c4.wallpaperflare.com/wallpaper/503/469/209/8-bit-city-buildings-artist-wallpaper-preview.jpg"
+                  className="spaceimage"
+                  src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1ec0373a-f986-4c65-a356-0136b27553e6/d9r4bk8-6133b056-ef9c-462e-816a-1eac494c78d5.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMWVjMDM3M2EtZjk4Ni00YzY1LWEzNTYtMDEzNmIyNzU1M2U2XC9kOXI0Yms4LTYxMzNiMDU2LWVmOWMtNDYyZS04MTZhLTFlYWM0OTRjNzhkNS5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.xOmx1rewR3TUkG-GTK1LbGo7KinBjU0wNewmhUYXC7Q"
                 />
-              </p>
-            </div>
-            <div className="story-div">
-              <p className="story-text">
-                <p>
-                  For a brief time there was peace and prosperity, until
-                  inevitable human greed reshaped the solar system.
-                </p>
 
-                <img
-                  className="Rossimage"
-                  src="https://i.pinimg.com/originals/83/80/6c/83806c2d045fd0d7b0be73c6357708b9.png"
-                />
-                <p> A brilliant and evil engineer from Holtercorp™,</p>
-
-                <p className="RosstheBoss">Ross Miglin</p>
-              </p>
-            </div>
-            <div className="story-div">
-              <p className="story-text">
-                <p>
-                  The Evil Ross used alien computer technology and blockchain
-                  algorithms to devalue all the worlds currency and replaced it
-                  with ℞osscoin.
-                </p>
-                <p>
-                  Overnight he became the wealthiest person in history and every
-                  nation on earth became a third world country.
-                </p>
-                <p>
-                  Ross used his infinite resources to build a mega fortress on
-                  Pluto and put a ℞60 Billion bounty on his own life, just to
-                  troll everyone.
-                </p>
-              </p>
-            </div>
-            <div className="story-div">
-              <p className="story-text">
-                <p>The world responded the only way it could:</p>
-                <p>
-                  by devolving into a mercenary driven society ruled by whoever
-                  had the biggest and coolest giant robot.{" "}
-                </p>
-                <img
-                  className="mechimage"
-                  src="https://i.pinimg.com/originals/d7/ad/b0/d7adb06c596788cfbfbd86628d6b59ae.png"
-                />
-                <p>
-                  Perhaps one day a brave adventurer will build a robot so bad
-                  ass that they can travel all the way to Pluto and bring the
-                  world’s justice to Ross.
-                </p>
-              </p>
-            </div>
-            <div className="story-div">
-              <p className="story-text">
-                Until that day, humanity dreams and waits....
-              </p>
-              <img
-                className="spaceimage"
-                src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1ec0373a-f986-4c65-a356-0136b27553e6/d9r4bk8-6133b056-ef9c-462e-816a-1eac494c78d5.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMWVjMDM3M2EtZjk4Ni00YzY1LWEzNTYtMDEzNmIyNzU1M2U2XC9kOXI0Yms4LTYxMzNiMDU2LWVmOWMtNDYyZS04MTZhLTFlYWM0OTRjNzhkNS5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.xOmx1rewR3TUkG-GTK1LbGo7KinBjU0wNewmhUYXC7Q"
-              />
-
-              <div className="robot-selector-div">
-                <Link to="/robots" className="robot-link">
-                  Choose a Robot
-                </Link>
+                <div className="robot-selector-div">
+                  <Link to="/robots" className="robot-link">
+                    Choose a Robot
+                  </Link>
+                </div>
               </div>
             </div>
           </Slider>
